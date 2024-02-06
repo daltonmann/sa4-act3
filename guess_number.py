8093a3e4bd0799c89917e5a1257690ec72e61d0c
Dalton Mann <daltonjamann@gmail.com>
2024-02-06 12:02:19 -0600

    Added in a higher/lower hint for incorrect guesses

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,17 @@
-number = "10"
+number = 10
 
 print("I'm thinking of a number...")
 guess = input("What number am I thinking of? ")
 
 while 1 == 1:
+    if guess == "q":
+        print(f"Sorry! The number was {number}.")
+        break
+    guess = int(guess)
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
-    elif guess == "q":
-        print(f"Sorry! The number was {number}.")
-        break
-    guess = input("Try again...")
+    if guess > number:
+        guess = input("Your guess is too high. Try again...")
+    else:
+        guess = input("Your guess is too low. Try again...")
